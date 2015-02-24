4c2c5f748ec274266cda299c0951f8930979e1f6
DilipKuki <dilip.vns4@gmail.com>
2015-02-24 17:11:18 +0530

    Switch to the part 2

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../app.js');
  
 describe('GET /', function() {
   it('respond with hello world', function(done) {
-    request(app).get('/').expect('hello world Test Jenkins and check connectivity', done);
+    request(app).get('/').expect('hello world Test Jenkins and check connectivity part 2 ...', done);
   });
 });
